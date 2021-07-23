2d977bb40436ba53d175124438647f3abdc9d882
Guiu Mateu <gmateu@simon.es>
2021-07-23 20:39:38 +0200

    feat(xercise/1.basic/2.hashes)

--- 1.basic/2.hashes/exercise/zomes/exercise/src/lib.rs
+++ 1.basic/2.hashes/exercise/zomes/exercise/src/lib.rs
@@ -10,10 +10,47 @@ pub struct Book {
 
 #[hdk_extern]
 pub fn add_book(external_input: Book) -> ExternResult<EntryHash> { 
-    unimplemented!()
+    // let book: Book = external_input;
+    // match create_entry(&book) {
+    //     Ok(x) => x,
+    //     Err(e) => panic!("{}", e)
+    // };
+
+    let book: Book = external_input;
+    let _header_hash: HeaderHash = create_entry(&book)?;
+
+    let entry_hash: EntryHash = hash_entry(&book)?;
+
+    Ok(entry_hash)
 }
 
 #[hdk_extern]
 pub fn get_book(external_input: EntryHash) -> ExternResult<Book> {
-    unimplemented!()
+    let entry_hash: EntryHash = external_input;
+    // let element: Element = match get(entry_hash.into_hash(), GetOptions::default()) {
+    //     Ok(entry_hash_option) => { match entry_hash_option {
+    //         Some(entry_hash) => entry_hash,
+    //         None => panic!("Book not found")
+    //     }},
+    //     Err(e) => panic!("Error: {}", e)
+    // };
+
+    let element: Element = get(entry_hash.into_hash(), GetOptions::default())?
+        .ok_or(WasmError::Guest(String::from("Could not find book")))?;
+
+    // let book: Book = match element.entry().to_app_option() {
+    //     Ok(book_option) => { match book_option {
+    //         Some(book) => book,
+    //         None => panic!("No book in option")
+    //     }},
+    //     Err(e) => panic!("Error {}", e)
+    // };
+
+    // let book_option: Option<Book> = element.entry().to_app_option()?;
+    // let book: Book = book_option.expect("No book in option");
+
+    let book: Book = element.entry().to_app_option()?
+        .ok_or(WasmError::Guest(String::from("No book found")))?;
+
+    Ok(book)
 }
