7322473b96e82b48464da89f2793a20eb63446df
Guiu Mateu <gmateu@simon.es>
2021-07-23 20:39:23 +0200

    feat(xercise/1.basic/1.entries)

--- 1.basic/1.entries/exercise/zomes/exercise/src/lib.rs
+++ 1.basic/1.entries/exercise/zomes/exercise/src/lib.rs
@@ -1,9 +1,19 @@
+use hdk::prelude::*;
+
+entry_defs![Greeting::entry_def()];
+
+// Define struct forn an entry
+#[hdk_entry(id = "gretting")]
+pub struct Greeting(String);
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct SomeExternalInput {
     content: String,
 }
 
+// Create entry
+#[hdk_extern]
 pub fn say_greeting(input: SomeExternalInput) -> ExternResult<HeaderHash> {
-    unimplemented!()
+    let grett: Greeting = Greeting(input.content);
+    create_entry(grett)
 }
